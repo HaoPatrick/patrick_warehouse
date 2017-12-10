54d461981603fef349073dcd9452af4fd5e1e6a6
haoxiangpeng <3150102255@zju.edu.cn>
2017-12-09 22:31:22 -0800

    :art: refactor

--- backend/main.py
+++ backend/main.py
@@ -1,35 +1,9 @@
-from flask import request, jsonify
-from backend import views, error_handler
+from flask import jsonify
 from backend import app
+from backend.views import post_weather_data
 
-
-# app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-  return 'Hello World!'
-
-
-@app.route('/weather', methods=['POST', 'GET'])
-def post_weather_data():
-  if request.method == 'POST':
-    try:
-      views.add_weather(request.form['temp'], request.form['humidity'])
-    except Exception as e:
-      return e
-    return jsonify({
-      'response': 'ok'
-    })
-  elif request.method == 'GET':
-    try:
-      weather = views.get_weather_limited(request.args['limit'])
-    except KeyError:
-      raise error_handler.InvalidParameter("invalid parameter")
-    return jsonify({
-      'response': 'ok',
-      'weather': weather
-    })
-
+app.add_url_rule('/', 'home', lambda: jsonify({'response': 'ok', 'message': 'hey, patrick\'s data warehouse'}))
+app.add_url_rule('/weather', 'post_weather', post_weather_data)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,6 @@
-from .db_connection import WeatherDB
+from flask import request, jsonify
+from backend.db_connection import WeatherDB
+from backend.error_handler import InvalidParameter
 
 
 def add_weather(temp: float, humidity: float):
@@ -11,3 +13,23 @@ def get_weather_limited(limit_to: int) -> list:
   rv = weather_db.get_data_limited(limit_to)
   rv = map(lambda x: {'humidity': x[1], 'temp': x[2], 'date': x[3]}, rv)
   return list(rv)
+
+
+def post_weather_data():
+  if request.method == 'POST':
+    try:
+      add_weather(request.form['temp'], request.form['humidity'])
+    except Exception as e:
+      return e
+    return jsonify({
+      'response': 'ok'
+    })
+  elif request.method == 'GET':
+    try:
+      weather = get_weather_limited(request.args['limit'])
+    except KeyError:
+      raise InvalidParameter("invalid parameter")
+    return jsonify({
+      'response': 'ok',
+      'weather': weather
+    })
